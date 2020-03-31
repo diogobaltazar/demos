b7b1f9b17776ee48acd61c599a3d4e995627e2f3
diogo <d.ogobaltazar+github@gmail.com>
2020-03-31 02:53:58 +0200

    add cli exercises

---
+++ azure/cli/exercises/install_cli.sh
@@ -0,0 +1,4 @@
+# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-apt?view=azure-cli-latest
+# install az at /bin/az and azure-cli to /home/user/azure-cli
+curl -L https://aka.ms/InstallAzureCli | bash
+
---
+++ azure/cli/exercises/uninstall_cli.sh
@@ -0,0 +1,5 @@
+rm -r home/user/azure-cli
+rm /bin/az
+
+# rm from .bashrc:
+# source '/home/diogo/azure-cli/az.completion'
