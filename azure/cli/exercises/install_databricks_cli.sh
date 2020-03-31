653283f653f1a164525745b00d63b8f01b6710fb
diogo <d.ogobaltazar+github@gmail.com>
2020-03-31 14:01:42 +0200

    add exercises

---
+++ azure/cli/exercises/install_databricks_cli.sh
@@ -0,0 +1,6 @@
+# install
+sudo pip install databricks-cli
+
+# gen databricks-cli dedicated token on databricks workspace/user: TOKEN 
+# get databricks host: https://<region>.azuredatabricks.net
+databricks configure --token
