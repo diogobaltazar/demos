d394c85b26977e46c0e0e6e7e9505841d61670b3
diogo <d.ogobaltazar+github@gmail.com>
2020-04-06 16:19:45 +0200

    add exercise

---
+++ programming/exercises/crt_python_dist_package.sh
@@ -0,0 +1,5 @@
+# MODULES read https://docs.python.org/3/tutorial/modules.html#packages
+# BUILD and DIST py dists (SETUP) https://packaging.python.org/key_projects/#setuptools
+# LICENSING https://choosealicense.com/
+
+# TODO 
