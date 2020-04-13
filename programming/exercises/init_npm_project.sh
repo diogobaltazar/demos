7af33dd7aa3c8a18b66a188c84fc2f1e43f162f7
diogo <d.ogobaltazar+github@gmail.com>
2020-04-13 02:28:30 +0200

    add npm exercises

---
+++ programming/exercises/init_npm_project.sh
@@ -0,0 +1,4 @@
+
+
+echo '> install express locally and add it as dependecy to package.json'
+sudo npm install express -save
---
+++ programming/exercises/install_npm_dependencies.sh
@@ -0,0 +1,3 @@
+echo '> inside node project folder'
+echo '> install project dependencies as in package-json'
+npm install
