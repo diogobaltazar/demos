4d93f17b89817e7b6d4335d7ce063a936982c873
diogo <d.ogobaltazar+github@gmail.com>
2020-07-26 18:16:26 +0200

    complete

--- azure/exercises/get_sp_credentials.sh
+++ azure/exercises/get_sp_credentials.sh
@@ -1,4 +1,6 @@
 # go to AD
 # search for all applications
 # select SP
-# @endpoints: OAuth 2.0 token endpoint (v1)
+# @refresh.url  : OAuth 2.0 token endpoint (v1)
+# @client.id    : Application (client) ID
+# @credential   : <on-creation>
