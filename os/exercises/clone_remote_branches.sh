5e078ba90c8a3acaa4aa9976118a03266df24d6e
Diogo Walter de Freitas <diogobaltazar@users.noreply.github.com>
2020-09-15 23:14:55 +0200

    Create clone_remote_branches.sh

---
+++ os/exercises/clone_remote_branches.sh
@@ -0,0 +1,9 @@
+# clone repo: git clone <repo-url>
+
+# shows only master
+git branch
+
+# shows all remote branches
+git branch -a
+
+# create a copy of a specific branch: git checkout <specific-remote-branch>
