725c9d112e73c5091d61b5f42f198848f73f5984
dipm <dipm@novonordisk.com>
2020-11-09 23:45:51 +0100

    add

---
+++ spark/exercises/create_dataframe.sh
@@ -0,0 +1,17 @@
+
+```python
+# pyspark
+
+```
+
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html
+```python
+# pandas
+df = pd.DataFrame({'a': [1, 2, 3], 'b': ['a', 'b', 'c']})
+
+```
+
+```sql
+
+
+```
