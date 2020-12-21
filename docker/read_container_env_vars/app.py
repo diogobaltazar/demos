0cc45db354f97f0c2928848f4cf20d2abc7aafcc
diogo <d.ogobaltazar+github@gmail.com>
2020-12-22 00:13:21 +0100

    add demo

---
+++ docker/read_container_env_vars/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+color = 'red'
+
+@app.route("/")
+def main():
+    # TODO
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = '8080')
+
